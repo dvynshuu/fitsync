876a7bf7b14270ff0e4abbfff2203a97d8567607
dvynshuu <officialdivyanshu94@gmail.com>
2024-11-24 11:14:23 +0530

    Added All Files

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import Hero from "../src/components/hero";
+import Workout from "./components/Workout";
+import Generator from "./components/Generator";
+import { generateWorkout } from "./utils/functions";
+
+function App() {
+  const [workout, setWorkout] = useState(null);
+  const [poison, setPoison] = useState("individual");
+  const [muscles, setMuscles] = useState([]);
+  const [goal, setGoals] = useState("strength_power");
+
+  function updateWorkout() {
+    if(muscles.length <1) {
+      return
+    }
+    let newWorkout = generateWorkout({poison, muscles, goal});
+    console.log(newWorkout)
+    setWorkout(newWorkout);
+
+    window.location.href = "#workout";
+  }
+  return (
+    <main
+      className="min-h-screen flex flex-col bg-gradient-to-r
+     from-slate-800 to-slate-950 text-white text-sm sm:text-base"
+    >
+      <Hero />
+      <Generator poison={poison}
+       setPoison={setPoison}
+       muscles={muscles}
+       setMuscles={setMuscles}
+       goal={goal}
+       setGoals={setGoals}
+       updateWorkout={updateWorkout} 
+     />
+      {workout && <Workout workout={workout} />}
+    </main>
+  );
+}
+
+export default App;
